2345abd96e5daa3e28edbea1409b2739e5a71005
myyana04 <70784143+myyana04@users.noreply.github.com>
2020-10-14 23:05:12 -0400

    Add files via upload

---
+++ HomeWork8Main.java
@@ -0,0 +1,13 @@
+public class HomeWork8Main {
+
+    public static void main(String[] args) {
+        HomeWork8 a1=new HomeWork8();
+
+    //method for findout about toner count in a printer;
+        a1.tonerCount(8);
+        a1.pagesCount(11);
+
+
+
+    }
+}
