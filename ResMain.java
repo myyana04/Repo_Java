d1e048fa1a943cfe43b57a6bff3076dd5c2926ae
myyana04 <70784143+myyana04@users.noreply.github.com>
2020-11-05 00:11:49 -0500

    Add files via upload

--- ResMain.java
+++ ResMain.java
@@ -1,10 +1,25 @@
 public class ResMain {
+
     public static void main(String[] args) {
-        Restaurant a1=new Restaurant();
-        a1.isAvailable(26);
+
+        Restaurant r1=new Restaurant("Naan Shack",35);
+        r1.isAvailable();
+        r1.seatsParty(10);
+        r1.printsRestaurantSummary();
+        r1.isAvailable();
+        r1.printsRestaurantSummary();
+        r1.seatsParty(2);
+        r1.printsRestaurantSummary();
+        r1.seatsParty(22);
+        r1.printsRestaurantSummary();
+        r1.removesParty(33);
+        r1.printsRestaurantSummary();
+
+    }
+
 
 
 
 
     }
-}
+
