1cf9a6a430c5e5339b6d1de396a5863d90397845
myyana04 <70784143+myyana04@users.noreply.github.com>
2020-12-07 16:53:25 -0500

    Add files via upload

---
+++ Hardly_Trying.java
@@ -0,0 +1,11 @@
+package TryingHard;
+
+public class Hardly_Trying {
+    public static void main(String[] args) {
+        String townName="My town name is Sylhet";
+        char chrAt9=townName.charAt(9);
+        System.out.println(chrAt9);
+
+
+    }
+}
