5b60d9a32e3c19a3cbb16325b0841b4ac4bf3dcf
Rasel Chowdhury <raselchowdhury@Rasels-MBP.home>
2021-05-02 17:43:57 -0400

    git commands

---
+++ src/NewMasterHw7.java
@@ -0,0 +1,17 @@
+public class NewMasterHw7 {
+
+    /**
+     * To view branches in local
+     *         git branch-a
+     *
+     *  To View local branches
+     *         git branch
+     *
+     *  Commands to create a branch
+     *         git branch Hw7     (branch name Hw7)
+     *
+     *  To switch Master
+     *         git checkout branchName    (Hw7)
+     *
+     */
+}
